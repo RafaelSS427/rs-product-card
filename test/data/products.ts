373da7d26f522056b843e9545850dd0a6af41a89
Rafael Sequeira Sandoval <rafaels@grupomundial.cr>
2022-06-12 22:05:47 -0600

    Version 0.0.1 lista

---
+++ test/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+    id: '1',
+    title: 'Coffee Mug - Sin img',
+}
+
+export const product2 = {
+    id: '2',
+    title: 'Coffee Mug - Con img',
+    img: './coffee-mug.png'
+}
